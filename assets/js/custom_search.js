5f59774e1e9f03f7de5736e500de1938fad40275
Приданников Михаил <p.mihail174@yandex.ru>
2026-01-30 13:01:05 +0500

    v3.1.0

--- assets/js/custom_search.js
+++ assets/js/custom_search.js
@@ -4,10 +4,10 @@
  * Кастомный поиск с дополнительными возможностями.
  *
  * @author      Mihail Pridannikov
- * @copyright   2021-2023, Mihail Pridannikov
+ * @copyright   2021-2025, Mihail Pridannikov
  * @license MIT
- * @version     3.0.0
- * @release     June 2023
+ * @version     3.1.0
+ * @release     May 28, 2025
  * @link        https://github.com/mihail-174/custom_search
  *
  */
@@ -38,12 +38,14 @@ window.CustomSearch = function(customSettings) {
 
     const FORM = document.querySelectorAll('.form-search');
     const DEFAULT_SETTINGS = {
+        mocks: true,
         clear: true,
         placeholder: 'Поиск',
     }
     let settings = this.deepMergeObjects(DEFAULT_SETTINGS, customSettings);
 
     this.init = function () {
+
         if (FORM.length) {
             FORM.forEach(form => {
                 if (form) {
@@ -58,7 +60,6 @@ window.CustomSearch = function(customSettings) {
                     form.querySelector('.form__input').addEventListener('input', e => this.handlerChangeOnTextField(e, form));
 
                     this.setPlaceholder(form);
-                    // this.setSettingsSearchInHeader(form);
 
                 }
             });
@@ -67,18 +68,13 @@ window.CustomSearch = function(customSettings) {
             }, true);
         }
     }
-
-    // событие на изменение разрешения браузера
     this.handlerResizeWindow = function () {
         FORM.forEach(form => {
             if (form) {
                 this.setPlaceholder(form);
-                // this.setSettingsSearchInHeader(form);
             }
         });
     }
-
-    // задаем плейсхолдер
     this.setPlaceholder = function (search) {
         switch (typeof settings.placeholder) {
             case 'string':
@@ -92,20 +88,29 @@ window.CustomSearch = function(customSettings) {
                 break;
         }
     }
-
-    // событие при изменении поля
     this.handlerChangeOnTextField = function (e, form) {
         // добавляем кнопку очистить поле
         if (e.currentTarget.value !== '') {
             this.addButtonClear(form);
             this.addClassFilled(form);
+            this.initSearchResult(form, e.currentTarget.value.toLowerCase());
         } else {
             this.removeButtonClear(form);
             this.removeClasses(form);
+            settings.mocks ? this.closePopover(form) : this.removeResults(form);
+        }
+
+    }
+    this.initSearchResult = function (form, query) {
+        if (settings.mocks) {
+            this.openPopover(form);
+        } else {
+            let words = query.split(' ');
+            words.filter(word => {
+                this.getData(form, word);
+            });
         }
     }
-
-    // добавляем кнопку очистить поле
     this.addButtonClear = function (form) {
         if (settings.clear) {
             if (!form.querySelector('.form__button-clear')) {
@@ -120,40 +125,91 @@ window.CustomSearch = function(customSettings) {
             }
         }
     }
-
-    // удаляем кнопку очистить поле
     this.removeButtonClear = function (search) {
         if (search.querySelector('.form__button-clear')) {
             search.querySelector('.form__button-clear').remove();
             search.classList.remove('is-button-clear');
         }
     }
-
-    // событие клика по кнопке очистить поле
     this.handlerClickOnButtonClear = function (e, form) {
         e.preventDefault();
         form.querySelector('.form__input').value = '';
         this.removeClasses(form);
-        this.removeResult(form);
+        settings.mocks ? this.closePopover(form) : this.removeResults(form);
         this.removeButtonClear(form);
     }
-
-    // удаление классов
     this.removeClasses = function (search) {
-        search.classList.remove('is-results');
         search.classList.remove('is-focus');
         search.classList.remove('is-open');
         search.classList.remove('is-filled');
     }
-
-    // добавляем класс что поле заполнено
     this.addClassFilled = function (form) {
         form.classList.add('is-filled');
     }
-
-    // удаляем результаты поиска
-    this.removeResult = function (search) {
-        search.querySelector('.search-result') ? search.querySelector('.search-result').remove() : null;
+    this.createPopover = function (form, data) {
+        if (!form.querySelector('.form__field-result')) {
+            form.querySelector('.form__fields').insertAdjacentHTML('beforeend', `
+                <div class="form__field form__field-result">
+                    <div class="search-result-popover">
+                        <div class="search-result-popover__content"></div>
+                    </div>
+                </div>
+            `);
+        }
+        const field = form.querySelector('.form__field-result');
+        this.clearResults(field);
+        this.insertResults(form, data);
+    }
+    this.openPopover = function (form) {
+        form.classList.add('is-popover-opened');
+    }
+    this.closePopover = function (form) {
+        form.classList.remove('is-popover-opened');
+    }
+    this.insertResults = function (form, data) {
+        if (data) {
+            data.map(item => {
+                form.querySelector('.search-result-popover__content').insertAdjacentHTML('beforeend',
+                    `
+                    <div class="search-result-mini__item">
+                        <div class="teaser-product-search">
+                            <div class="teaser-product-search__image">
+                                <img src="" alt="" width="32" height="32" loading="lazy">
+                            </div>
+                            <div class="teaser-product-search__name">Тестовое название товара</div>
+                            <div class="teaser-product-search__price">1 000р.</div>
+                        </div>
+                    </div>
+                `);
+            });
+        } else {
+            form.querySelector('.search-result-popover__content').textContent = 'Результаты не найдены';
+        }
+        this.openPopover(form);
+    }
+    this.clearResults = function (field) {
+        field.querySelector('.search-result-popover__content').innerHTML = '';
+    }
+    this.removeResults = function (form) {
+        if (form.querySelector('.form__field-result')) {
+            form.querySelector('.form__field-result').remove();
+        }
+    }
+    this.getData = function (form, query) {
+        fetch('/rest/search?key=' + query, {method: 'GET'})
+            .then((response) => {
+                if (response.ok) {
+                    return response.json()
+                }
+                throw new Error('Network response was not ok')
+            })
+            .then((data) => {
+                // console.log('DATA:', data);
+                this.createPopover(form, data);
+            })
+            .catch((error) => {
+                console.log('ERROR:', error);
+            })
     }
 
     if (FORM) {
