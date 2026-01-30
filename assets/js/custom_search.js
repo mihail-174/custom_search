ed760ca6f8c02a2e626b749d9339e349c637fce8
Приданников Михаил <p.mihail174@yandex.ru>
2026-01-30 12:15:25 +0500

    v3.0.0

--- assets/js/custom_search.js
+++ assets/js/custom_search.js
@@ -6,41 +6,60 @@
  * @author      Mihail Pridannikov
  * @copyright   2021, Mihail Pridannikov
  * @license MIT
- * @version     2.0.0
+ * @version     3.0.0
  * @release     November 2021
  * @link        https://github.com/mihail-174/custom_search
  *
  */
 
-window.CustomSearch = function(settings) {
-    const FORM = document.querySelectorAll('.form-search');
-    const SETTINGS = {
-        clear: true,
-        placeholderDesktop: 'Введите наименование',
-        placeholderMobile: 'Поиск',
-        placeholderBreakpoint: 767,
-        headerType: 'full',
-        headerSlideDirection: null,
+window.CustomSearch = function(customSettings) {
+    this.deepMergeObjects = function (obj1, obj2) {
+        const result = {};
+        for (const key in obj2) {
+            if (obj2.hasOwnProperty(key)) {
+                if (typeof obj2[key] === "object" && obj1.hasOwnProperty(key) && typeof obj1[key] === "object") {
+                    result[key] = this.deepMergeObjects(obj1[key], obj2[key]);
+                } else {
+                    result[key] = obj2[key];
+                }
+            }
+        }
+        for (const key in obj1) {
+            if (obj1.hasOwnProperty(key) && !result.hasOwnProperty(key)) {
+                if (typeof obj1[key] === "object") {
+                    result[key] = this.deepMergeObjects(obj1[key], {});
+                } else {
+                    result[key] = obj1[key];
+                }
+            }
+        }
+        return result;
     }
 
-    // начало
+    const FORM = document.querySelectorAll('.form-search');
+    const DEFAULT_SETTINGS = {
+        clear: true,
+        placeholder: 'Поиск',
+    }
+    let settings = this.deepMergeObjects(DEFAULT_SETTINGS, customSettings);
+
     this.init = function () {
         if (FORM.length) {
             FORM.forEach(form => {
                 if (form) {
 
                     // добавляем кнопку очистить поле
-                    if (this.getParameter('clear')) {
-                        if (form.querySelector('.form__input').value !== '') {
-                            this.addButtonClear(form);
-                        }
+                    if (form.querySelector('.form__input').value !== '') {
+                        this.addButtonClear(form);
+                        this.addClassFilled(form);
                     }
 
                     // событие при изменении поля
                     form.querySelector('.form__input').addEventListener('input', e => this.handlerChangeOnTextField(e, form));
 
                     this.setPlaceholder(form);
-                    this.setSettingsSearchInHeader(form);
+                    // this.setSettingsSearchInHeader(form);
+
                 }
             });
             window.addEventListener('resize', e => {
@@ -54,92 +73,20 @@ window.CustomSearch = function(settings) {
         FORM.forEach(form => {
             if (form) {
                 this.setPlaceholder(form);
-                this.setSettingsSearchInHeader(form);
+                // this.setSettingsSearchInHeader(form);
             }
         });
     }
 
-    // задаем настройки поиску находящийся в шапке
-    this.setSettingsSearchInHeader = function (form) {
-        if (this.getParameter('headerType') && !this.getParameter('headerBreakpoint')) {
-            this.setTypeSearch(form);
-            this.setSlideDirection(form);
-        } else {
-            let widthWindow = document.body.getBoundingClientRect().width;
-            this.removeClassTypeSearch(form);
-            this.removeClassSlideDirection(form);
-            this.getParameter('headerBreakpoint').map(item => {
-                let min = typeof item.breakpoint[0] === 'number' ? item.breakpoint[0] : rangeDevices[item.breakpoint[0]].value;
-                let max = typeof item.breakpoint[1] === 'number' ? item.breakpoint[1] : rangeDevices[item.breakpoint[1]].value;
-                if (min === 0 && widthWindow <= max) {
-                    this.setTypeSearch(form, item);
-                    item.type === 'short' && this.setSlideDirection(form, item);
-                } else if (max === 0 && widthWindow > min) {
-                    this.setTypeSearch(form, item);
-                    item.type === 'short' && this.setSlideDirection(form, item);
-                } else if (widthWindow > min && widthWindow <= max) {
-                    this.setTypeSearch(form, item);
-                    item.type === 'short' && this.setSlideDirection(form, item);
-                }
-            })
-        }
-    }
-
-    // устанавливаем тип поиска
-    this.setTypeSearch = function (form, item) {
-        if (this.getParameter('headerType') && !this.getParameter('headerBreakpoint')) {
-            form.classList.add(`form-search_type_${this.getParameter('headerType')}`)
-        } else {
-            form.classList.add(`form-search_type_${item.type}`)
-        }
-    }
-
-    // устанавливаем направление поиска если он "short"
-    this.setSlideDirection = function (form, item) {
-        if (this.getParameter('headerType') && !this.getParameter('headerBreakpoint')) {
-            this.getParameter('headerType') === 'short' ? form.classList.add(`form-search_slide-direction_${this.getParameter('headerSlideDirection')}`) : null;
-        } else {
-            form.classList.add(`form-search_slide-direction_${item.slideDirection}`)
-        }
-    }
-
-    this.removeClassTypeSearch = function (form) {
-        form.classList.remove(...[...form.classList].filter(n => n.startsWith('form-search_type_')));
-    }
-    this.removeClassSlideDirection = function (form) {
-        form.classList.remove(...[...form.classList].filter(n => n.startsWith('form-search_slide-direction_')));
-    }
-
-    // проверка на наличие кастомных настроек
-    this.checkingSettings = function () {
-        // return typeof settings !== 'undefined' || (typeof settings === 'object' && Object.keys(settings).length > 0);
-        return (typeof settings === 'object' && Object.keys(settings).length > 0);
-    }
-
-    // проверка на наличе параметра в кастомных настройках
-    this.checkingParameter = function (value) {
-        return typeof settings[value] !== 'undefined'
-    }
-    // получаем параметр из кастомных настроек, если там нету, то из значений по умолчанию
-    this.getParameter = function (value) {
-        // console.log(value, this.checkingSettings() && this.checkingParameter(value) ? settings[value] : SETTINGS[value])
-        return this.checkingSettings() && this.checkingParameter(value) ? settings[value] : SETTINGS[value];
-    }
-
     // задаем плейсхолдер
     this.setPlaceholder = function (search) {
-        switch (typeof this.getParameter('placeholderBreakpoint')) {
-            case 'number':
-                search.querySelector('.form__input').setAttribute('placeholder', document.body.getBoundingClientRect().width <= this.getParameter('placeholderBreakpoint') ? this.getParameter('placeholderMobile') : this.getParameter('placeholderDesktop'));
-                break;
+        switch (typeof settings.placeholder) {
             case 'string':
-                search.querySelector('.form__input').setAttribute('placeholder', document.body.getBoundingClientRect().width <= rangeDevices[this.getParameter('placeholderBreakpoint')].value ? this.getParameter('placeholderMobile') : this.getParameter('placeholderDesktop'));
+                search.querySelector('.form__input').setAttribute('placeholder', settings.placeholder);
                 break;
             case 'object':
-                let checkRange = this.getParameter('placeholderBreakpoint').some(function (item) {
-                    return typeof item === 'string' ? document.body.classList.contains(`js-device-${item}`) : null;
-                });
-                search.querySelector('.form__input').setAttribute('placeholder', checkRange ? this.getParameter('placeholderMobile') : this.getParameter('placeholderDesktop'));
+                let checkRange = Object.entries(settings.placeholder).find(item => document.body.classList.contains(`js-device-${item[0]}`));
+                search.querySelector('.form__input').setAttribute('placeholder', checkRange ? checkRange[1] : 'Поиск');
                 break;
             default:
                 break;
@@ -149,24 +96,37 @@ window.CustomSearch = function(settings) {
     // событие при изменении поля
     this.handlerChangeOnTextField = function (e, form) {
         // добавляем кнопку очистить поле
-        if (this.getParameter('clear')) {
-            e.currentTarget.value !== '' ? this.addButtonClear(form) : this.removeButtonClear(form);
+        if (e.currentTarget.value !== '') {
+            this.addButtonClear(form);
+            this.addClassFilled(form);
+        } else {
+            this.removeButtonClear(form);
+            this.removeClasses(form);
         }
     }
 
     // добавляем кнопку очистить поле
     this.addButtonClear = function (form) {
-        if (!form.querySelector('.form__button-clear')) {
-            form.querySelector('.form__field-query').insertAdjacentHTML('beforeend', '<button class="form__button-clear">Очистить</button>');
-            form.classList.add('is-button-clear');
-            form.querySelector('.form__button-clear').addEventListener('click', e => this.handlerClickOnButtonClear(e, form));
+        if (settings.clear) {
+            if (!form.querySelector('.form__button-clear')) {
+                form.querySelector('.form__field-query').insertAdjacentHTML('beforeend',
+                    `<button class="form__button-clear">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16">
+                        <path fill="gray" d="M1.793 1.796a1 1 0 000 1.414l4.793 4.793-4.793 4.793a1 1 0 001.414 1.414L8 9.417l4.793 4.793a1 1 0 001.414-1.414L9.414 8.003l4.793-4.793a1 1 0 00-1.414-1.414L8 6.589 3.207 1.796a1 1 0 00-1.414 0z" />
+                    </svg>
+                </button>`);
+                form.classList.add('is-button-clear');
+                form.querySelector('.form__button-clear').addEventListener('click', e => this.handlerClickOnButtonClear(e, form));
+            }
         }
     }
 
     // удаляем кнопку очистить поле
     this.removeButtonClear = function (search) {
-        search.querySelector('.form__button-clear').remove();
-        search.classList.remove('is-button-clear');
+        if (search.querySelector('.form__button-clear')) {
+            search.querySelector('.form__button-clear').remove();
+            search.classList.remove('is-button-clear');
+        }
     }
 
     // событие клика по кнопке очистить поле
@@ -183,6 +143,12 @@ window.CustomSearch = function(settings) {
         search.classList.remove('is-results');
         search.classList.remove('is-focus');
         search.classList.remove('is-open');
+        search.classList.remove('is-filled');
+    }
+
+    // добавляем класс что поле заполнено
+    this.addClassFilled = function (form) {
+        form.classList.add('is-filled');
     }
 
     // удаляем результаты поиска
